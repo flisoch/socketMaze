36dc8c9767b86bf50126539d873f6feee6cf6636
flisoch <flisochcs@gmail.com>
2018-12-22 23:29:27 +0300

    delete comments

--- mazeServer/src/main/java/app/GameConfig.java
+++ mazeServer/src/main/java/app/GameConfig.java
@@ -8,40 +8,20 @@ public class GameConfig {
     }
 
     private HostConnection hostConnection;
-//    private int serverIp;
-//    private int serverPort;
     private String serverPassword;
 
     public boolean isConfigured(){
         return serverPassword != null;
 
-//        return (serverIp != 0) && (serverPort != 0) && (serverPassword != null);
     }
 
     public GameConfig(HostConnection hostConnection) {
         this.hostConnection = hostConnection;
     }
 
-
-   /* public int getServerIp() {
-        return serverIp;
-    }
-
-    public int getServerPort() {
-        return serverPort;
-    }*/
-
     public String getServerPassword() {
         return serverPassword;
     }
-
-    /*public void setServerIp(int serverIp) {
-        this.serverIp = serverIp;
-    }
-
-    public void setServerPort(int serverPort) {
-        this.serverPort = serverPort;
-    }*/
 
     public void setServerPassword(String serverPassword) {
         this.serverPassword = serverPassword;
