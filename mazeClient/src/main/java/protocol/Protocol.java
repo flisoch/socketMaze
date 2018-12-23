0e81f36b6ed618b900bab40f4e03d9df5d138263
flisoch <flisochcs@gmail.com>
2018-12-23 23:44:47 +0300

    add commands to the protocol

--- mazeClient/src/main/java/protocol/Protocol.java
+++ mazeClient/src/main/java/protocol/Protocol.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class Protocol {
-    public enum Command{CREATE_SERVER, SAVE_SERVER_CONFIGURATION, DISCONNECT_MAIN};
+    public enum Command{CREATE_SERVER, SAVE_SERVER_CONFIGURATION, DISCONNECT_MAIN, GET_SERVERS, CHECK_PASSWORD, END_MESSAGE};
 }
