2362ab2cf2a91e728c64117fdb71ec21bb4cbf5d
flisoch <flisochcs@gmail.com>
2018-12-23 18:24:20 +0300

    add Builder annotation, delete getters, setters, add mazeHeight field

--- mazeClient/src/main/java/Menu/Server.java
+++ mazeClient/src/main/java/Menu/Server.java
@@ -1,18 +1,20 @@
 package Menu;
 
-import lombok.Data;
+import lombok.*;
 
 import java.net.InetAddress;
 
 @Data
+@Builder
+@ToString
+@AllArgsConstructor
 public class Server {
     private InetAddress address;
     private int port;
-
     private String name;
     private String password;
-
     private int maxPlayers;
     private int playersCount;
+    private int mazeHeight;
 
 }
