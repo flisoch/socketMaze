98a820ecebec1b01f3183c90eb53b1f4185f596d
flisoch <flisochcs@gmail.com>
2018-12-23 19:59:16 +0300

    add Disconnect message to commands enum

--- mazeClient/src/main/java/protocol/Protocol.java
+++ mazeClient/src/main/java/protocol/Protocol.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class Protocol {
-    public enum Command{CREATE_SERVER, SAVE_SERVER_CONFIGURATION};
+    public enum Command{CREATE_SERVER, SAVE_SERVER_CONFIGURATION, DISCONNECT_MAIN};
 }
