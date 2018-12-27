200c66da732d0376d77ef897570728dbae682c74
flisoch <flisochcs@gmail.com>
2018-12-28 02:21:35 +0300

    add KillserverCommand,handling of pythonClient requests related to players info, gamePassing info(finish time, ready for game status)

--- mazeServer/src/main/java/app/Server.java
+++ mazeServer/src/main/java/app/Server.java
@@ -1,32 +1,28 @@
 package app;
 
-import designParts.Observer;
 import protocol.Command;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Server implements Runnable {
 
     private ServerSocket serverSocket;
     private List<Connection> connections;
     private Thread gameServerThread;
+    private String[][] table;
 
     public ServerSocket getServerSocket() {
         return serverSocket;
     }
 
-    public Server(int port) throws IOException {
+    public  Server(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         connections = new ArrayList<>();
         gameServerThread = new Thread(this::run);
@@ -35,7 +31,9 @@ public class Server implements Runnable {
 
     @Override
     public void run() {
-        while (true) {
+        //todo:kill server somehow
+        AtomicBoolean gameFinished = new AtomicBoolean(false);
+        while (!gameFinished.get()) {
             try {
                 Socket socket = serverSocket.accept();
 
@@ -48,9 +46,15 @@ public class Server implements Runnable {
                         PrintWriter writer = new PrintWriter(socket.getOutputStream());
                         String s = br.readLine();
                         while (s != null) {
-                            onNext(s, writer);
+                            if (s.equals(Command.KILL_SERVER.name())) {
+                                gameFinished.set(true);
+                                break;
+                            }
+                            onNext(s, writer, connection);
                             s = br.readLine();
                         }
+                        System.out.println("SERVER KILLED " + serverSocket.getInetAddress().getHostAddress() + " "
+                                + serverSocket.getLocalPort());
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -62,11 +66,12 @@ public class Server implements Runnable {
         }
     }
 
-    private void onNext(String message, PrintWriter writer) {
+    private void onNext(String message, PrintWriter writer, Connection connection) {
 
         String[] parts = message.split(" ", 2);
         String commandFromClient = parts[0];
         String data;
+        long playerId;
 
         System.out.println(commandFromClient);
         Optional<Command> optionalCommand = Arrays.stream(Command.values()).filter(command -> command.name().equalsIgnoreCase(commandFromClient)).findAny();
@@ -76,42 +81,22 @@ public class Server implements Runnable {
                 case SAVE_TABLE_MAZE:
                     data = parts[1];
                     System.out.println(data);
-                    String[] lineParts = data.split(";");
-                    String port = lineParts[0];
-                    String table = lineParts[1];
-                    int host_port = Integer.parseInt(port.split(":")[1]);
+                    String table = data;
                     String[] lines = table.split(":")[1].split("//");
                     int n = lines.length - 1;
-                    String[][] mazeTable = new String[n][n];
-                    Connection connection = null;
-                    for (Connection connection1 : connections) {
-                        if (connection1.socket.getLocalPort() == host_port) {
-                            connection = connection1;
-                        }
-                    }
+                    this.table = new String[n][n];
+
 
                     for (int i = 0; i < n; i++) {
                         String[] items = lines[i].split(",");
                         for (int j = 0; j < items.length; j++) {
-                            mazeTable[i][j] = items[j];
+                            this.table[i][j] = items[j];
                         }
-                    }
-                    if (connection != null) {
-                        connection.setMazeTable(mazeTable);
                     }
                     break;
                 case GET_TABLE_MAZE:
-                    data = parts[1];
-                    String[] port1 = data.split(":");
-                    int host_port1 = Integer.parseInt(port1[1]);
-                    Connection connection1 = connections.stream()
-                            .filter(connection2 -> connection2.socket.getLocalPort() == host_port1)
-                            .findAny()
-                            .orElse(null);
-                    String[][] tableMaze1 = connection1.getMazeTable();
-
                     StringBuilder builder = new StringBuilder();
-                    for (String[] row : tableMaze1) {
+                    for (String[] row : this.table) {
                         for (String item : row) {
                             builder.append(item).append(",");
                         }
@@ -119,24 +104,68 @@ public class Server implements Runnable {
                         builder.append("//");
                     }
                     builder.setLength(builder.length() - 3);
+                    //todo:check if works without \n
                     builder.append('\n');
                     System.out.println(builder);
                     writer.println(builder.toString());
                     writer.flush();
                     break;
+                case SEND_TIME:
+                    data = parts[1];
+                    String[] timeAndPlayerId = data.split(",");
+                    double finishTime = Double.parseDouble(timeAndPlayerId[0].split(":")[1]);
+                    playerId = Long.parseLong(timeAndPlayerId[1].split(":")[1]);
+                    connection = connections.stream()
+                            .filter(connection3 -> connection3.getPlayerId() == playerId)
+                            .findAny()
+                            .orElse(null);
+                    connection.setFinishTime(finishTime);
+                    break;
+                case READY:
+                    data = parts[1];
+                    playerId = Long.parseLong(data.split(":")[1]);
+                    connection = connections.stream()
+                            .filter(connection4 -> connection4.getPlayerId() == playerId)
+                            .findAny()
+                            .orElse(null);
+                    connection.isReady = true;
+
+
+                    boolean ready = false;
+                    while (!ready) {
+                        ready = connections.stream()
+                                .filter(Connection::isReady)
+                                .count() == connections.size();
+                        if(!ready){
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    writer.println("READY");
+                    writer.flush();
+                    break;
+
+                case GET_RESULTS:
+                    connections.sort(Comparator.comparingDouble(Connection::getFinishTime));
+                    connections.forEach(connection1 -> writer.println(
+                            "player:" + connection1.getPlayerName() + "," +
+                                    "time:" + connection1.getFinishTime()
+                    ));
+                    writer.flush();
+                    break;
+                case CREATE_PLAYER_ID:
+                    data = parts[1];
+                    String playerName = data.split(":")[1];
+                    System.out.println(playerName);
+                    connection.setPlayerName(playerName);
+                    connection.setPlayerId(System.currentTimeMillis());
+                    System.out.println(connection.getPlayerId());
+                    writer.println(connection.getPlayerId());
+                    writer.flush();
             }
         }
-        //send message to all except source
-
-        /*for (Socket s : sockets) {
-            try {
-                PrintWriter pw = new PrintWriter(s.getOutputStream());
-                pw.println(message);
-                pw.flush();
-//                pw.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
     }
 }
