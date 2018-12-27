7de5110d5fdad9f40cd18869fe6532d540209b7a
flisoch <flisochcs@gmail.com>
2018-12-28 02:13:15 +0300

    remove redundant commands

--- mazeClient/src/main/java/protocol/Protocol.java
+++ mazeClient/src/main/java/protocol/Protocol.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class Protocol {
     public enum Command{CREATE_SERVER, SAVE_SERVER_CONFIGURATION, DISCONNECT_MAIN, GET_SERVERS, CHECK_PASSWORD,
-        END_MESSAGE, SAVE_TABLE_MAZE};
+        END_MESSAGE};
 }
