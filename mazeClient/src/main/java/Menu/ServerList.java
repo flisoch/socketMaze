83930d55cef83df8a78b2e71e25da734d188a9b5
flisoch <flisochcs@gmail.com>
2018-12-28 05:19:00 +0300

    check: if null-command, return empty list

--- mazeClient/src/main/java/Menu/ServerList.java
+++ mazeClient/src/main/java/Menu/ServerList.java
@@ -13,13 +13,11 @@ import java.util.Properties;
 
 public class ServerList {
 
-    private static List<Server> servers;
+//    private static List<Server> servers;
 
     public static List<Server> getServers(){
-        if(servers == null){
-            servers = getServersFromServer();
-        }
-        return servers;
+
+        return getServersFromServer();
     }
 
     private static List<Server> getServersFromServer() {
@@ -31,40 +29,46 @@ public class ServerList {
 
         List<Server> servers = new ArrayList<>();
         String response = MainServerConnector.getResponse();
-        String[] lines = response.split("\n");
-        for(String line: lines){
-
-            Server server = Server.builder().build();
-            String[] attributes = line.split(",");
-
-            for (String attribute:attributes){
-                String[] parts = attribute.split(":");
-                String field = parts[0];
-                String data = parts[1];
-                switch (field) {
-                    case "ip":
-                        try {
-                            server.setAddress(InetAddress.getByName(data));
-                        } catch (UnknownHostException e) {
-                            e.printStackTrace();
-                        }
-                        break;
-                    case "port":
-                        server.setPort(Integer.parseInt(data));
-                        break;
-                    case "name":
-                        server.setName(data);
-                        break;
-                    case "maxPlayers":
-                        server.setMaxPlayers(Integer.parseInt(data));
-                        break;
-                    case "playersCount":
-                        server.setPlayersCount(Integer.parseInt(data));
-                        break;
-                }
-            }
-            servers.add(server);
+        if(response.equals("NULL")){
+            return servers;
         }
+        else {
+            String[] lines = response.split("\n");
+            for(String line: lines){
+
+                Server server = Server.builder().build();
+                String[] attributes = line.split(",");
+
+                for (String attribute:attributes){
+                    String[] parts = attribute.split(":");
+                    String field = parts[0];
+                    String data = parts[1];
+                    switch (field) {
+                        case "ip":
+                            try {
+                                server.setAddress(InetAddress.getByName(data));
+                            } catch (UnknownHostException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        case "port":
+                            server.setPort(Integer.parseInt(data));
+                            break;
+                        case "name":
+                            server.setName(data);
+                            break;
+                        case "maxPlayers":
+                            server.setMaxPlayers(Integer.parseInt(data));
+                            break;
+                        case "playersCount":
+                            server.setPlayersCount(Integer.parseInt(data));
+                            break;
+                    }
+                }
+                servers.add(server);
+            }
+        }
+
 
         return servers;
 
